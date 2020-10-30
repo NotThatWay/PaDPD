e812e6df041a279ade52a2cfaaf748c4cd4eb2f7
Khripach <haosfortum.random@gmail.com>
2020-10-30 20:30:24 +0300

    Scripted auto-commit on change (2020-10-30 20:30:23) by gitwatch.sh

--- lab2/src/main/java/lab2_package/Main.java
+++ lab2/src/main/java/lab2_package/Main.java
@@ -5,18 +5,19 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.err.println("Usage: Main <passage path> <aiports> <output path>");
+            System.err.println("Usage: Main <passages path> <aiports path> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();
         job.setJarByClass(Main.class);
         job.setJobName("Passages timeout analyzing");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileInputFormat.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.setMapperClass(AirportMapper.class);
         job.setReducerClass(WordReducer.class);
