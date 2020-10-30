d8f8be73fcbd28350de776377f30ab1c174aed1b
Khripach <haosfortum.random@gmail.com>
2020-10-30 20:20:19 +0300

    Scripted auto-commit on change (2020-10-30 20:20:19) by gitwatch.sh

--- lab2/src/main/java/lab2_package/AirportMapper.java
+++ lab2/src/main/java/lab2_package/AirportMapper.java
@@ -7,9 +7,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, RaceWritableComparable, Text>  {
+    private static final String COMMA = ",";
+    private static final String VERTICAL_SLASH = "|";
+
     public void map(LongWritable key, Text value, Context context) throws
             IOException, InterruptedException {
         String line = value.toString();
-        String[] words = line.replaceFirst()
+        String[] words = line.replaceFirst(COMMA, VERTICAL_SLASH).split(VERTICAL_SLASH);
     }
 }
