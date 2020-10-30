6fce02d5f8160137e0be25c343b1062ad773ebf4
Khripach <haosfortum.random@gmail.com>
2020-10-30 21:11:13 +0300

    Scripted auto-commit on change (2020-10-30 21:11:13) by gitwatch.sh

--- lab2/src/main/java/lab2_package/AirportPartitioner.java
+++ lab2/src/main/java/lab2_package/AirportPartitioner.java
@@ -5,6 +5,6 @@ import org.apache.hadoop.io.Text;
 
 public class AirportPartitioner extends Partitioner<PassageWritableComparable, Text> {
     public int getPartition(PassageWritableComparable key, Text value, int numPartitions) {
-        
+        return key.getCode() % numPartitions;
     }
 }
