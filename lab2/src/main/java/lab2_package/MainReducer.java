2cad3c36f280bd9a284e1f51d99e8016d75145e6
Khripach <haosfortum.random@gmail.com>
2020-10-30 21:53:28 +0300

    Scripted auto-commit on change (2020-10-30 21:53:28) by gitwatch.sh

--- lab2/src/main/java/lab2_package/MainReducer.java
+++ lab2/src/main/java/lab2_package/MainReducer.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
 public class MainReducer extends Reducer<PassageWritableComparable, Text, Text, Text> {
     protected void reduce(PassageWritableComparable key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
-        Iterator iter = values.iterator();
+        Iterator<Text> iter = values.iterator();
         Text airport_name = new Text(iter.next());
         while (iter.hasNext()) {
 
