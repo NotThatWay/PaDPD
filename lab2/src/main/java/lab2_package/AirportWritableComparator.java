986de37b364e73d9df3c0e6747b7d28920bd34ed
Khripach <haosfortum.random@gmail.com>
2020-10-30 21:25:43 +0300

    Scripted auto-commit on change (2020-10-30 21:25:43) by gitwatch.sh

--- lab2/src/main/java/lab2_package/AirportWritableComparator.java
+++ lab2/src/main/java/lab2_package/AirportWritableComparator.java
@@ -11,8 +11,12 @@ public class AirportWritableComparator extends WritableComparator {
     public int compare(WritableComparable a, WritableComparable b) {
         PassageWritableComparable first = (PassageWritableComparable) a ;
         PassageWritableComparable second = (PassageWritableComparable) b ;
-        if (first > second) {
-            
+        if (first.getCode() > second.getCode()) {
+            return 1;
         }
+        else if (first.getCode() < second.getCode()) {
+            return -1;
+        }
+        return 0;
     }
 }
