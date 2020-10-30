a960391bb248f02294c25d0247d4d6c6ab31755c
Khripach <haosfortum.random@gmail.com>
2020-10-30 21:31:07 +0300

    Scripted auto-commit on change (2020-10-30 21:31:07) by gitwatch.sh

--- lab2/src/main/java/lab2_package/MainReducer.java
+++ lab2/src/main/java/lab2_package/MainReducer.java
@@ -3,6 +3,12 @@ package lab2_package;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class MainReducer extends Reducer<PassageWritableComparable, Text, Text, Text> {
-    protected void reduce(PassageWritableComparable, Iterable<Text>)
+    protected void reduce(PassageWritableComparable key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        
+
+    }
 }
