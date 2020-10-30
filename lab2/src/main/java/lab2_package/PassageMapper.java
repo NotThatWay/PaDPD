4cc104864dbcb2bb98d88b455faaf4057953bd4b
Khripach <haosfortum.random@gmail.com>
2020-10-30 23:23:25 +0300

    Scripted auto-commit on change (2020-10-30 23:23:25) by gitwatch.sh

--- lab2/src/main/java/lab2_package/PassageMapper.java
+++ lab2/src/main/java/lab2_package/PassageMapper.java
@@ -16,7 +16,7 @@ public class PassageMapper extends Mapper<LongWritable, Text, PassageWritableCom
             IOException, InterruptedException {
         String line = value.toString();
         String[] words = line.split(COMMA, -1);
-        if (key.get() != 0 && !words[ARR_DELAY_NEW_NUMBER].equals(NO_DELAY)) {
+        if (key.get() != 0 && !words[ARR_DELAY_NEW_NUMBER].equals(NO_DELAY) && !words[ARR_DELAY_NEW_NUMBER].isEmpty()) {
             context.write(new PassageWritableComparable(Integer.parseInt(words[ARR_DELAY_NEW_NUMBER]), 1), new Text(words[DEST_AIRPORT_ID_NUMBER]));
         }
     }
