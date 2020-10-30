f426481c9e1df5973c3961ce9e5828e935ecbc50
Khripach <haosfortum.random@gmail.com>
2020-10-30 20:10:14 +0300

    Scripted auto-commit on change (2020-10-30 20:10:14) by gitwatch.sh

--- lab2/src/main/java/lab2_package/AirportMapper.java
+++ lab2/src/main/java/lab2_package/AirportMapper.java
@@ -5,4 +5,5 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportMapper extends Mapper<LongWritable, Text, RaceWritableComparable, Text> {
+    public void map()
 }
