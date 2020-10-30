2b6dd65ef7d79cb03f1330d7edb9465e0282ba20
Khripach <haosfortum.random@gmail.com>
2020-10-30 19:25:05 +0300

    Scripted auto-commit on change (2020-10-30 19:25:05) by gitwatch.sh

--- lab2/src/main/java/lab2_package/RaceWritableComparable.java
+++ lab2/src/main/java/lab2_package/RaceWritableComparable.java
@@ -1,6 +1,7 @@
 package lab2_package;
 
 import org.apache.hadoop.io.WritableComparable;
+import java.io.DataOutput;
 
 public class RaceWritableComparable implements WritableComparable {
     public int compareTo(Object o) {
