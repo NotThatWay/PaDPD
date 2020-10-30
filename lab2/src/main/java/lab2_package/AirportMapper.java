574a3557ce6f32691f56fd62a983207a5c44bf16
Khripach <haosfortum.random@gmail.com>
2020-10-30 23:39:14 +0300

    Scripted auto-commit on change (2020-10-30 23:39:14) by gitwatch.sh

--- lab2/src/main/java/lab2_package/AirportMapper.java
+++ lab2/src/main/java/lab2_package/AirportMapper.java
@@ -14,7 +14,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, PassageWritableCom
     public void map(LongWritable key, Text value, Context context) throws
             IOException, InterruptedException {
         String line = value.toString();
-        String[] words = line.replaceFirst(COMMA, VERTICAL_SLASH).replaceAll(QUOTE).split(VERTICAL_SLASH);
+        String[] words = line.replaceFirst(COMMA, VERTICAL_SLASH).replaceAll(QUOTE, "").split(VERTICAL_SLASH);
         if (key.get() != 0) {
             context.write(new PassageWritableComparable(Integer.parseInt(words[0]), 0), new Text(words[1]));
         }
