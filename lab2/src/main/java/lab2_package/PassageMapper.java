064721db9358768e174558dd72a3eb9df7e5c2e8
Khripach <haosfortum.random@gmail.com>
2020-10-30 20:35:01 +0300

    Scripted auto-commit on change (2020-10-30 20:35:00) by gitwatch.sh

--- lab2/src/main/java/lab2_package/PassageMapper.java
+++ lab2/src/main/java/lab2_package/PassageMapper.java
@@ -1,4 +1,14 @@
 package lab2_package;
 
-public class PassageMapper extends Mapper<> {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class PassageMapper extends Mapper<LongWritable, Text, PassageWritableComparable, Text> {
+    public void map(LongWritable key, Text value, Context context) throws
+            IOException, InterruptedException {
+        
+    }
 }
