2fff06550a6ebb8be6297ab64b6576dd2d55dfe7
Khripach <haosfortum.random@gmail.com>
2020-10-30 21:22:05 +0300

    Scripted auto-commit on change (2020-10-30 21:22:05) by gitwatch.sh

--- lab2/src/main/java/lab2_package/AirportWritableComparator.java
+++ lab2/src/main/java/lab2_package/AirportWritableComparator.java
@@ -1,7 +1,10 @@
 package lab2_package;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportWritableComparator extends WritableComparator {
     public int compare(WritableComparable a, WritableComparable b) {
+        PassageWritableComparable 
+    }
 }
