ebb87c50ae8bff693b8a4b31c5f30f83125af026
Khripach <haosfortum.random@gmail.com>
2020-10-30 21:51:05 +0300

    Scripted auto-commit on change (2020-10-30 21:51:05) by gitwatch.sh

--- lab2/src/main/java/lab2_package/MainReducer.java
+++ lab2/src/main/java/lab2_package/MainReducer.java
@@ -12,7 +12,7 @@ public class MainReducer extends Reducer<PassageWritableComparable, Text, Text, 
         Iterator iter = values.iterator();
         Text airport_name = new Text(iter.next());
         while (iter.hasNext()) {
-
+            
         }
 
     }
