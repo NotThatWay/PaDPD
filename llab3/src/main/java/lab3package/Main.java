bb49c4980845f684dd7f8cf85079be1f8a5b91fd
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-04 22:22:15 +0300

    Scripted auto-commit on change (2020-12-04 22:22:15) by gitwatch.sh

--- llab3/src/main/java/lab3package/Main.java
+++ llab3/src/main/java/lab3package/Main.java
@@ -17,7 +17,8 @@ public class Main {
     private static final int AIRPORT_ID_COLUMN = 0;
     private static final int AIRPORT_NAME_COLUMN = 1;
     private static final String FLIGHTS_TITLE = "\"YEAR\",\"QUARTER\",\"MONTH\",\"DAY_OF_MONTH\",\"DAY_OF_WEEK\"";
-    private static final int AIRPORT_ORIGIN = 1;
+    private static final int AIRPORT_ORIGIN = 11;
+    private static final int AIRPORT_DESTINATION = 14;
 
 
 
@@ -30,7 +31,8 @@ public class Main {
                 .map(x -> x.replaceFirst(AIR_PRE_DELIMETER, AIR_FINAL_DELIMETER).split(AIR_FINAL_DELIMETER))
                 .mapToPair(x -> new Tuple2<>(Integer.parseInt(x[AIRPORT_ID_COLUMN]), x[AIRPORT_NAME_COLUMN]));
         JavaPairRDD<Tuple2<Integer,Integer>,FlightsSerializable> delaysAndCancelled = flights.filter(x -> x.contains(FLIGHTS_TITLE))
-                .map(x -> x.split(FLIGHT_DELIMETER)).mapToPair(x -> new Tuple2<>(Integer.parseInt(x[])))
+                .map(x -> x.split(FLIGHT_DELIMETER))
+                .mapToPair(x -> new Tuple2<>(Integer.parseInt(x[AIRPORT_ORIGIN]), Integer.parseInt(x[AIRPORT_DESTINATION])), FlightsSerializable.countDelays)
     }
 }
 
