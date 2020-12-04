10c72c7faf08117cb9935c46e68637f79c5a14ff
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-04 22:06:55 +0300

    Scripted auto-commit on change (2020-12-04 22:06:55) by gitwatch.sh

--- llab3/src/main/java/lab3package/Main.java
+++ llab3/src/main/java/lab3package/Main.java
@@ -11,9 +11,9 @@ public class Main {
     private static final String AIRPORTS_FILE = "airports.csv";
     private static final String FLIGHTS_FILE = "passages.csv";
     private static final String AIRPORTS_TITLE = "Code,Description";
-    private static final String PRE_DELIMETER = ",";
-    private static final String FINAL_DELIMETER = "%::#%;##%#";
-    private static final String DELIMETER = "%::#%;##%#";
+    private static final String AIR_PRE_DELIMETER = ",";
+    private static final String AIR_FINAL_DELIMETER = "%::#%;##%#";
+    private static final String DELIMETER = ",";
     private static final int AIRPORT_ID_COLUMN = 0;
     private static final int AIRPORT_NAME_COLUMN = 1;
     private static final String FLIGHTS_TITLE = "\"YEAR\",\"QUARTER\",\"MONTH\",\"DAY_OF_MONTH\",\"DAY_OF_WEEK\"";
@@ -25,7 +25,7 @@ public class Main {
         JavaRDD<String> airports = sc.textFile(AIRPORTS_FILE);
         JavaRDD<String> flights = sc.textFile(FLIGHTS_FILE);
         JavaPairRDD<Integer,String> airportPair = airports.filter(x -> !x.contains(AIRPORTS_TITLE))
-                .map(x -> x.replaceFirst(PRE_DELIMETER, FINAL_DELIMETER).split(FINAL_DELIMETER))
+                .map(x -> x.replaceFirst(AIR_PRE_DELIMETER, AIR_FINAL_DELIMETER).split(AIR_FINAL_DELIMETER))
                 .mapToPair(x -> new Tuple2<>(Integer.parseInt(x[AIRPORT_ID_COLUMN]), x[AIRPORT_NAME_COLUMN]));
         JavaPairRDD<Tuple2<Integer,Integer>,FlightsSerializable> delaysAndCancelled = flights.filter(x -> x.contains())
                 .split("")
