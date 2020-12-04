991fb1a45635f4589d1eb8ef089768ce0d4214a3
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-04 20:55:51 +0300

    Scripted auto-commit on change (2020-12-04 20:55:51) by gitwatch.sh

--- llab3/src/main/java/lab3package/Main.java
+++ llab3/src/main/java/lab3package/Main.java
@@ -7,12 +7,15 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class Main {
+    private static final String AIRPORTS_FILE = "airports.csv";
+    private static final String FLIGHTS_FILE = "passages.csv";
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> airports = sc.textFile("airports.csv");
-        JavaRDD<String> flights = sc.textFile("passages.csv");
-        JavaPairRDD<Long,String> =
+        JavaRDD<String> airports = sc.textFile(AIRPORTS_FILE);
+        JavaRDD<String> flights = sc.textFile(FLIGHTS_FILE);
+        JavaPairRDD<Long,String> = 
 
     }
 }
