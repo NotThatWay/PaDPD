976070f3c29a6c3282648ae314d2e728749272c9
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-04 20:44:16 +0300

    Scripted auto-commit on change (2020-12-04 20:44:16) by gitwatch.sh

--- llab3/src/main/java/lab3package/Main.java
+++ llab3/src/main/java/lab3package/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String>
+        JavaRDD<String> 
     }
 }
 
