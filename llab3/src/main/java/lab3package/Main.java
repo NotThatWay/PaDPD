20d1a72fbc16d07394ec65a3f9812e7cb68ed2ff
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-04 21:14:44 +0300

    Scripted auto-commit on change (2020-12-04 21:14:44) by gitwatch.sh

--- llab3/src/main/java/lab3package/Main.java
+++ llab3/src/main/java/lab3package/Main.java
@@ -10,13 +10,17 @@ public class Main {
     private static final String AIRPORTS_FILE = "airports.csv";
     private static final String FLIGHTS_FILE = "passages.csv";
     private static final String TITLE = "Code,Description";
+    private static final String PRE_DELIMETER = ",";
+    private static final String FINAL_DELIMETER = "$%#$%#$#%#";
+
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(AIRPORTS_FILE);
         JavaRDD<String> flights = sc.textFile(FLIGHTS_FILE);
-        JavaPairRDD<Long,String> airportPair = airports.filter(x -> !x.contains(TITLE)).replaceFirst()
+        JavaPairRDD<Long,String> airportPair = airports.filter(x -> !x.contains(TITLE))
+                .map(x -> x.replaceFirst(PRE_DELIMETER, FINAL_DELIMETER)).replaceAll()
 
     }
 }
