4d6fc2f111183b0ea601d7892a994de5a95de7b1
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-04 21:48:33 +0300

    Scripted auto-commit on change (2020-12-04 21:48:33) by gitwatch.sh

--- llab3/src/main/java/lab3package/FlightsSerializable.java
+++ llab3/src/main/java/lab3package/FlightsSerializable.java
@@ -7,16 +7,18 @@ public class FlightsSerializable implements Serializable {
     public float maxDelay, percentDelay, percentCancelled;
     public FlightsSerializable countDelays(Iterator<String> flights) {
         float currDelay = 0;
-        int delays = 0, cancelled = 0;
+        int delays = 0, cancelled = 0, flightsNumber = 0;
         final static String NO_DELAY = "0.00";
         while (flights.hasNext()) {
             String flight = flights.next();
+            flightsNumber++;
             if (!flight.isEmpty()) {
                 cancelled++;
             }
             else if (flight.equals(NO_DELAY)) {
-                
+                delays++;
             }
+            else
             currDelay = Float.parseFloat(flight);
         }
     }
