f059569f8eb3bb2752c5bdff40e5b97036fa8d84
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 19:19:38 +0300

    Scripted auto-commit on change (2020-12-12 19:19:38) by gitwatch.sh

--- lab4/src/main/java/lab4package/ExecuteMessage.java
+++ lab4/src/main/java/lab4package/ExecuteMessage.java
@@ -3,11 +3,14 @@ package lab4package;
 public class ExecuteMessage {
     public String id;
     public String code;
-    public String 
+    public String functionName;
+    public Test[] tests;
 
-    public ExecuteMessage(String id, String code) {
+    public ExecuteMessage(String id, String code, String functionName, Test[] tests) {
         this.id = id;
         this.code = code;
+        this.functionName = functionName;
+        this.tests = tests;
     }
 
     public String toString() {
