1cf6ba7651cae598cb8fa213732af565bc9a23e5
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 18:39:36 +0300

    Scripted auto-commit on change (2020-12-12 18:39:36) by gitwatch.sh

--- lab4/src/main/java/lab4package/ResultsStoreActor.java
+++ lab4/src/main/java/lab4package/ResultsStoreActor.java
@@ -17,7 +17,7 @@ public class ResultsStoreActor extends AbstractActor {
                 })
                 .match(RetrievedMessage.class, message -> {
                     System.out.println(message.toString());
-                    sender().tell(new StoredMessage(message.id, results.(message.id)), self());
+                    sender().tell(new StoredMessage(message.id, results.get(message.id)), self());
                 }).build();
     }
 }
