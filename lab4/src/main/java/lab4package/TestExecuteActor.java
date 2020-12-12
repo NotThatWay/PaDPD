afd8a83ba1a0f6cfb0705428762d07e2aa50ab2f
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 18:53:16 +0300

    Scripted auto-commit on change (2020-12-12 18:53:16) by gitwatch.sh

--- lab4/src/main/java/lab4package/TestExecuteActor.java
+++ lab4/src/main/java/lab4package/TestExecuteActor.java
@@ -3,11 +3,21 @@ package lab4package;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestExecuteActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(ExecuteMessage.class, message -> {
-                    System.out.printf("EXECUTE %s\n", message.toString())
+                    System.out.printf("EXECUTE %s\n", message.toString());
+                    Result result = ...;
+                    sender.tell(new StoredMessage(message.id, ))
                 })
+    }
+
+    public static Result executeTest(ExecuteMessage message) {
+        ScriptEngine = new ScriptEngineManager().getEngineByName("nashorn");
+        
     }
 }
