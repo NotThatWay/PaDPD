9f2a6366db46dbd92deed8bc815b9757b48a8546
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 18:46:43 +0300

    Scripted auto-commit on change (2020-12-12 18:46:43) by gitwatch.sh

--- lab4/src/main/java/lab4package/ExecuteMessage.java
+++ lab4/src/main/java/lab4package/ExecuteMessage.java
@@ -1,2 +1,4 @@
-package lab4package;public class ExecuteMessage {
+package lab4package;
+
+public class ExecuteMessage {
 }
--- lab4/src/main/java/lab4package/TestExecuteActor.java
+++ lab4/src/main/java/lab4package/TestExecuteActor.java
@@ -1,9 +1,11 @@
 package lab4package;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TestExecuteActor extends AbstractActor {
     public Receive createReceive() {
-        
+        return ReceiveBuilder.create()
+                .match()
     }
 }
