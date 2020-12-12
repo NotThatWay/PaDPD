ce798f0cf1396648162ee7bc14b7e21f034521ed
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 18:08:30 +0300

    Scripted auto-commit on change (2020-12-12 18:08:29) by gitwatch.sh

--- lab4/src/main/java/lab4package/ResultsStoreActor.java
+++ lab4/src/main/java/lab4package/ResultsStoreActor.java
@@ -2,9 +2,10 @@ package lab4package;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import Message;
 
 public class ResultsStoreActor extends AbstractActor {
     public Receive createReceive() {
-        return ReceiveBuilder.create().match()
+        return ReceiveBuilder.create().match(Message.class)
     }
 }
