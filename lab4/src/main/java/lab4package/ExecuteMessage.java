9d1a81795758d5d790fe8c088dc85186837d5f4d
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 18:49:09 +0300

    Scripted auto-commit on change (2020-12-12 18:49:09) by gitwatch.sh

--- lab4/src/main/java/lab4package/ExecuteMessage.java
+++ lab4/src/main/java/lab4package/ExecuteMessage.java
@@ -1,4 +1,13 @@
 package lab4package;
 
 public class ExecuteMessage {
+    public String id;
+
+    public ExecuteMessage(String id) {
+        this.id = id
+    }
+
+    public String toString() {
+        return id;
+    }
 }
