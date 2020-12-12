7fb769cf4aa4557d89a270456326cf81057e19b3
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 17:50:09 +0300

    Scripted auto-commit on change (2020-12-12 17:50:09) by gitwatch.sh

--- lab4/src/main/java/lab4package/Main.java
+++ lab4/src/main/java/lab4package/Main.java
@@ -4,6 +4,8 @@ import akka.actor.ActorSystem;
 
 public class Main {
     public static void main(String[] args) {
-        ActorSystem actorSystem = 
+        ActorSystem actorSystem = ActorSystem.create("lab4");
+
+        
     }
 }
