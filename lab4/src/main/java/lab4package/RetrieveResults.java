eea2e813bf4107df71505e4629a3c81b02e7feab
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-12 19:10:29 +0300

    Scripted auto-commit on change (2020-12-12 19:10:29) by gitwatch.sh

--- lab4/src/main/java/lab4package/RetrieveResults.java
+++ lab4/src/main/java/lab4package/RetrieveResults.java
@@ -2,6 +2,9 @@ package lab4package;
 
 public class RetrieveResults {
     public String id;
+    public 
 
-    public RetrieveResults(String id)
+    public RetrieveResults(String id) {
+        this.id = id;
+    }
 }
