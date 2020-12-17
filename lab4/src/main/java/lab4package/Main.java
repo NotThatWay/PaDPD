b0111fb9a0b641b461070f41106b6b34da1906ce
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-18 02:33:47 +0300

    Scripted auto-commit on change (2020-12-18 02:33:47) by gitwatch.sh

--- lab4/src/main/java/lab4package/Main.java
+++ lab4/src/main/java/lab4package/Main.java
@@ -2,20 +2,26 @@ package lab4package;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.util.concurrent.CompletionStage;
+
 public class Main {
+    static final String IP = 
+
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("lab4");
         Http http = Http.get(actorSystem);
         ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
         Server server = new Server(actorSystem);
         Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.getRoute().flow(actorSystem, actorMaterializer);
-        
+        CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost())
 
     }
 }
