43e9e8460110108fcb4bf2716afba7894ae9a66b
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-18 02:03:22 +0300

    Scripted auto-commit on change (2020-12-18 02:03:22) by gitwatch.sh

--- lab4/src/main/java/lab4package/Server.java
+++ lab4/src/main/java/lab4package/Server.java
@@ -18,6 +18,8 @@ public class Server {
     public Route getRoute() {
         Directives.route(Directives.path("execute", () ->
                 Directives.route(Directives.post(() ->
-                        Directives.entity(Jackson.unmarshaller(RetrieveResults.class))))))
+                        Directives.entity(Jackson.unmarshaller(RetrieveResults.class), body -> {
+                            
+                        })))))
     }
 }
