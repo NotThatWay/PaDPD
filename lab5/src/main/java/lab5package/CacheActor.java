e69ef68bfe748d1dccbfb6caf11809edb944c13a
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-19 02:00:47 +0300

    Scripted auto-commit on change (2020-12-19 02:00:47) by gitwatch.sh

--- lab5/src/main/java/lab5package/CacheActor.java
+++ lab5/src/main/java/lab5package/CacheActor.java
@@ -3,8 +3,14 @@ package lab5package;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
+
 public class CacheActor extends AbstractActor {
+    public HashMap<String, Integer> cache = new HashMap<>();
+
     public Receive createReceive() {
-        return ReceiveBuilder.create().match();
+        return ReceiveBuilder.create().match(ReceiveMessage.class, message -> {
+            getSender().tell()
+        });
     }
 }
