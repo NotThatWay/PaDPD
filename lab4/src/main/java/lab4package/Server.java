4889a73b2ce2d8861cf8fc44da1693a72cc56b91
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-18 03:34:53 +0300

    Scripted auto-commit on change (2020-12-18 03:34:53) by gitwatch.sh

--- lab4/src/main/java/lab4package/Server.java
+++ lab4/src/main/java/lab4package/Server.java
@@ -30,8 +30,8 @@ public class Server {
         return Directives.route(Directives.path("execute", () ->
                 Directives.route(Directives.post(() ->
                         Directives.entity(Jackson.unmarshaller(RetrieveResults.class), body -> {
-                            actorRef.tell(new ExecuteMessage(body.getId(), body.getFunctionName(), body.getJsScript(), body.getTests()), ActorRef.noSender());
-                            return Directives.complete(StatusCodes.OK, String.format("Package %s started\n", body.getId()));
+                            actorRef.tell(new ExecuteMessage(body.getPackageID(), body.getFunctionName(), body.getJsScript(), body.getTests()), ActorRef.noSender());
+                            return Directives.complete(StatusCodes.OK, String.format("Package %s started\n", body.getPackageID()));
                         })))),
                 Directives.path("retrieve", () -> Directives.route(Directives.get(() ->
                         Directives.parameter("packageID", id -> {
