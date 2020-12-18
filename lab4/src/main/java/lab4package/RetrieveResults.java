58a43cc55d97883117a9245bcc44130f080a8db4
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-18 03:04:23 +0300

    Scripted auto-commit on change (2020-12-18 03:04:23) by gitwatch.sh

--- lab4/src/main/java/lab4package/RetrieveResults.java
+++ lab4/src/main/java/lab4package/RetrieveResults.java
@@ -1,6 +1,22 @@
 package lab4package;
 
 public class RetrieveResults {
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    public void setJsScript(String jsScript) {
+        this.jsScript = jsScript;
+    }
+
+    public void setTests(Test[] tests) {
+        this.tests = tests;
+    }
+
     public String id;
 
     public String getId() {
