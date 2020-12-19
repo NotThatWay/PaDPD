bb8ff8b0a8019fdbf081fce5c82816eb851a0e0a
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-19 03:06:14 +0300

    Scripted auto-commit on change (2020-12-19 03:06:14) by gitwatch.sh

--- lab5/src/main/java/lab5package/Main.java
+++ lab5/src/main/java/lab5package/Main.java
@@ -79,7 +79,7 @@ public class Main {
             });
         }).map(pair -> {
             cache.tell(new StoredMessage(pair.getKey(), pair.getValue()), ActorRef.noSender());
-            return HttpResponse.create().withEntity()
+            return HttpResponse.create().withEntity(String.valueOf(pair.getValue()));
         })
     }
 }
