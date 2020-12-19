b2811a5dff149264efc9bea9e170c0d225678cdf
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-19 03:08:15 +0300

    Scripted auto-commit on change (2020-12-19 03:08:15) by gitwatch.sh

--- lab5/src/main/java/lab5package/StoredMessage.java
+++ lab5/src/main/java/lab5package/StoredMessage.java
@@ -2,9 +2,9 @@ package lab5package;
 
 public class StoredMessage {
     public String url;
-    public int time;
+    public Number time;
 
-    public StoredMessage(String url, int time) {
+    public StoredMessage(String url, Number time) {
         this.url = url;
         this.time = time;
     }
