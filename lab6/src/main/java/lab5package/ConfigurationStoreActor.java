91a52a7534025ce3633a15bed9f3f66d4d6a3aca
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-26 03:28:52 +0300

    Scripted auto-commit on change (2020-12-26 03:28:52) by gitwatch.sh

--- lab6/src/main/java/lab5package/ConfigurationStoreActor.java
+++ lab6/src/main/java/lab5package/ConfigurationStoreActor.java
@@ -1,5 +1,12 @@
 package lab5package;
 
-public class ConfigurationStoreActor {
-    
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+public class ConfigurationStoreActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create().match()
+
+    }
 }
