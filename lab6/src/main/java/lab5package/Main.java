b77237235ead0fa7fbe77e1a5424e2180c7b2e80
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-26 03:45:04 +0300

    Scripted auto-commit on change (2020-12-26 03:45:04) by gitwatch.sh

--- lab6/src/main/java/lab5package/Main.java
+++ lab6/src/main/java/lab5package/Main.java
@@ -1,14 +1,23 @@
 package lab5package;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import org.apache.zookeeper.*;
+import akka.http.javadsl.server.Directives;
 
 public class Main {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("routes");
         Http http = Http.get(actorSystem);
-        ActorRef actorRef = actorSystem.actorOf(Props.create(...));
+        ActorRef actorRef = actorSystem.actorOf(Props.create(ConfigurationStoreActor.class));
+        ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
+        Flow<HttpRequest, HttpResponse, NotUsed> route = 
     }
 }
