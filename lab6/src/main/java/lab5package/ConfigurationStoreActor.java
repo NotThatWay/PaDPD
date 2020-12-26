743968bc7fb937d6e3dce53c754fa509b3117d18
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-26 03:39:23 +0300

    Scripted auto-commit on change (2020-12-26 03:39:23) by gitwatch.sh

--- lab6/src/main/java/lab5package/ConfigurationStoreActor.java
+++ lab6/src/main/java/lab5package/ConfigurationStoreActor.java
@@ -1,21 +1,22 @@
 package lab5package;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class ConfigurationStoreActor extends AbstractActor {
-    ArrayList<String> servers = new ArrayList<String>();
+    ArrayList<String> servers = new ArrayList<>();
     Random random = new Random();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(ServersList.class, message -> {
                 servers = message.servers; }).match(ServerQuery.class, message -> {
-                    getSender().tell(servers.get(random.nextInt(servers.size())))
-        })
+                    getSender().tell(servers.get(random.nextInt(servers.size())), ActorRef.noSender());
+        }).build();
 
     }
 }
