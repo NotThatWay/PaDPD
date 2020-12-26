88413f9947c1c1b601e1da597d0c6813265fdb7b
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-26 03:27:17 +0300

    Scripted auto-commit on change (2020-12-26 03:27:17) by gitwatch.sh

--- lab6/src/main/java/lab5package/Main.java
+++ lab6/src/main/java/lab5package/Main.java
@@ -1,5 +1,14 @@
 package lab5package;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+
 public class Main {
-    public static void main(String args) 
+    public static void main(String[] args) {
+        ActorSystem actorSystem = ActorSystem.create("routes");
+        Http http = Http.get(actorSystem);
+        ActorRef actorRef = actorSystem.actorOf(Props.create(...));
+    } 
 }
