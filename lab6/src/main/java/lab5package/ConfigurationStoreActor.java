d566c95ba0e6f204614e6274e43249a9542276e3
Khripach Georgy <haosfortum.random@gmail.com>
2020-12-26 03:33:38 +0300

    Scripted auto-commit on change (2020-12-26 03:33:38) by gitwatch.sh

--- lab6/src/main/java/lab5package/ConfigurationStoreActor.java
+++ lab6/src/main/java/lab5package/ConfigurationStoreActor.java
@@ -3,10 +3,15 @@ package lab5package;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+
 public class ConfigurationStoreActor extends AbstractActor {
+    ArrayList<String> servers = new ArrayList<String>();
+
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(ServersList.class)
+        return ReceiveBuilder.create().match(ServersList.class, message -> {
+                servers = message.servers; }).match()
 
     }
 }
